d85511df3c92cdf1cda5f482e2fcefc098de456f
hasib <hasib.arman.08@gmail.com>
2020-09-23 22:59:16 +0600

    toolbar

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,20 +1,16 @@
 <template>
-  <div>
-      <ProductSection></ProductSection>
-  </div>
+    <div>
+        <ProductSection></ProductSection>
+        <we-plant-tree-map></we-plant-tree-map>
+    </div>
 </template>
 
 <script>
-
     import ProductSection from '../components/product-section';
+    import WePlantTreeMap from "../components/we-plant-tree-map";
 
-import WePlantTreeMap from "../components/we-plant-tree-map";
-export default {
-  name: 'Home',
-  components: {WePlantTreeMap},
-}
     export default {
-         name: 'Home',
-        components:{ProductSection},
-     }
+        name: 'Home',
+        components: {WePlantTreeMap,ProductSection},
+    }
 </script>
