5d0c337a2bc3e6f7f961cfcadd036825edbd1411
hasib <hasib.arman.08@gmail.com>
2020-09-23 10:36:47 +0600

    i am hasib

---
+++ front-end/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    i am hasib
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+}
+</script>
