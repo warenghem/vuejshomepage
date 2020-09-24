b87b6b021f697060d997a3391bec3ed18138ea2c
hasib <hasib.arman.08@gmail.com>
2020-09-24 23:23:27 +0600

    studio section

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -2,15 +2,17 @@
     <div>
         <ProductSection></ProductSection>
         <we-plant-tree-map></we-plant-tree-map>
+        <studio-section></studio-section>
     </div>
 </template>
 
 <script>
     import ProductSection from '../components/product-section';
     import WePlantTreeMap from "../components/we-plant-tree-map";
+    import StudioSection from "../components/studio-section";
 
     export default {
         name: 'Home',
-        components: {WePlantTreeMap,ProductSection},
+        components: {StudioSection, WePlantTreeMap,ProductSection},
     }
 </script>
