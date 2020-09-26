6eb7887df247aa541bdcee50c2b692babe5275a8
ashiqulemucse <ashiqulemu.cse@gmial.com>
2020-09-26 14:03:51 +0600

    # works on products section

--- front-end/src/components/product-section.vue
+++ front-end/src/components/product-section.vue
@@ -19,13 +19,14 @@
                     </div>
                     <div class="inner">
                         <div class="content">
-                            <small>Visuals to come</small>
-                            <h1>Wallet</h1>
-                            <p> Plant like its leaves. <br> Obviously.</p>
+                            <small>
+                                Disponible prochainement
+                            </small>
+                            <h1>Le porte-cartes</h1>
+                            <p> Indispensablement petit. Sans manquer de tanin. </p>
                             <article>
-                                Ally of your daily life, it is designed around oblique lines representing the emblematic
-                                W
-                                of the brand. Each of them has space for your cards.
+                                Plus pratique à porter, il complémente le portefeuille pour de la monnaie et quelques
+                                cartes lors de vos déplacements.
                             </article>
 
                         </div>
@@ -36,12 +37,11 @@
                     <div class="inner">
                         <div class="content">
                             <small>Visuals to come</small>
-                            <h1>Wallet</h1>
-                            <p> Plant like its leaves. <br> Obviously.</p>
+                            <h1>Le bracelet Apple Watch</h1>
+                            <p> Un objet high-tech. Soutenu par la nature.</p>
                             <article>
-                                Ally of your daily life, it is designed around oblique lines representing the emblematic
-                                W
-                                of the brand. Each of them has space for your cards.
+                                Inspiré par la haute horlogerie, sa boucle déployante et ses coutures renforcées
+                                anobliront la montre iconique Apple.
                             </article>
 
                         </div>
@@ -50,17 +50,16 @@
                 </div>
             </v-col>
 
-            <v-col cols="6" md="6" sm="12" class="parent-left">
+            <v-col cols="6" md="6" sm="12" class="parent-right">
                 <div class="horizontal">
                     <div class="inner">
                         <div class="content">
                             <small>Visuals to come</small>
-                            <h1>Wallet</h1>
-                            <p> Plant like its leaves. <br> Obviously.</p>
+                            <h1>La ceinture</h1>
+                            <p> Elle ne vous laissera pas tomber. Tout en noblesse.</p>
                             <article>
-                                Ally of your daily life, it is designed around oblique lines representing the emblematic
-                                W
-                                of the brand. Each of them has space for your cards.
+                                Large et épaisse, aux finitions sellier, elle s’integre aussi bien sur un costume que
+                                sur un jean.
                             </article>
 
                         </div>
@@ -70,13 +69,15 @@
                 <div class="vertical">
                     <div class="inner">
                         <div class="content">
-                            <small>Visuals to come</small>
-                            <h1>Wallet</h1>
-                            <p> Plant like its leaves. <br> Obviously.</p>
+                            <small>
+                                Disponible prochainement
+                            </small>
+                            <h1>Le sac 48H</h1>
+                            <p> Des vignes et du lin. Tel est son destin.</p>
                             <article>
-                                Ally of your daily life, it is designed around oblique lines representing the emblematic
-                                W
-                                of the brand. Each of them has space for your cards.
+                                Le plus grand des sacs. Composé de raisins et d’une doublure en lin francais.
+                                Indispensable pour le week-end, le sport, ou les courts séjours. C’est la pièce
+                                maitresse, qui donne le ton de la collection.
                             </article>
 
                         </div>
@@ -84,13 +85,15 @@
                     </div>
                     <div class="inner">
                         <div class="content">
-                            <small>Visuals to come</small>
-                            <h1>Wallet</h1>
-                            <p> Plant like its leaves. <br> Obviously.</p>
+                            <small>
+
+                                Visuels à venir
+                            </small>
+                            <h1>Le sac à dos</h1>
+                            <p> Pratique et elegant. Ce n’est pas incompatible.</p>
                             <article>
-                                Ally of your daily life, it is designed around oblique lines representing the emblematic
-                                W
-                                of the brand. Each of them has space for your cards.
+                                Composé de raisins et d’une doublure en lin francais. Indispensable pour vous déplacer
+                                en journée. Il est concu pour durer.
                             </article>
 
                         </div>
@@ -127,11 +130,88 @@
 
     .productSection {
         min-height: 100vh;
+        @media(max-width: 425px){
+            display: flex;
+            flex-direction: column;
+        }
         .parent-left {
-            .vertical{
+            @media(max-width: 425px){
+                 max-width: 100%;
+            }
+            .vertical {
                 height: 65%;
                 display: flex;
+                @media(max-width: 425px){
+                    height: auto;
+                    display: flex;
+                    flex-direction: column;
+                }
+                .inner {
+                    flex: 1;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    position: relative;
+                    height: 100%;
+                    &:nth-child(1) {
+                        background: url(../assets/images/WA-mif-seamless-REVISION.png);
+                    }
+                    &:nth-child(2) {
+                        background: url(../assets/images/PC-rendu-scaled.jpg);
+                    }
+                    .content {
+                        padding: 20px;
+                        text-align: center;
+                        small {
+                            color: #0083A4;
+                            font-family: 'teradeli-light', sans-serif !important;
+                            font-size: 13px;
+                        }
+                        h1 {
+                            color: #fff;
+                        }
+                        p {
+                            font-family: 'teradeli-book', sans-serif !important;
+                            font-size: 20px;
+                            color: rgba(255, 255, 255, .65) !important;
 
+                        }
+                        article {
+                            font-size: 14px;
+                            letter-spacing: 1px;
+                            opacity: 0;
+                            color: #fff;
+                        }
+
+                    }
+                    .overlay {
+                        position: absolute;
+                        background: #1f4046f2;
+                        opacity: 0;
+                        top: 0;
+                        left: 0;
+                        right: 0;
+                        bottom: 0;
+                        transition: opacity 0.4s ease-in-out;
+                    }
+                    &:hover {
+                        cursor: url(../assets/images/cross_100185.svg), auto;
+                        .overlay {
+                            opacity: 1;
+                        }
+                        .content {
+                            position: relative;
+                            z-index: 1;
+                            article {
+                                opacity: 1;
+                            }
+                        }
+                    }
+                }
+
+            }
+            .horizontal {
+                height: 35%;
                 .inner {
                     flex: 1;
                     display: flex;
@@ -167,34 +247,44 @@
                         }
 
                     }
-                    .overlay{
+                    .overlay {
                         position: absolute;
-                        background:#1f4046f2;
-                        opacity:0;
+                        background: #1f4046f2;
+                        opacity: 0;
                         top: 0;
                         left: 0;
                         right: 0;
                         bottom: 0;
                         transition: opacity 0.4s ease-in-out;
                     }
-                    &:hover{
-                        cursor: url(../assets/images/cross_100185.svg),auto;
-                        .overlay{
+                    &:hover {
+                        cursor: url(../assets/images/cross_100185.svg), auto;
+                        .overlay {
                             opacity: 1;
                         }
-                        .content{
+                        .content {
                             position: relative;
                             z-index: 1;
-                            article{
-                               opacity: 1;
+                            article {
+                                opacity: 1;
                             }
                         }
                     }
                 }
-
             }
-            .horizontal {
-                height: 35%;
+        }
+        .parent-right {
+            @media(max-width: 425px){
+                max-width: 100%;
+            }
+            .vertical {
+                height: 65%;
+                display: flex;
+                @media(max-width: 425px){
+                    height: auto;
+                    display: flex;
+                    flex-direction: column;
+                }
                 .inner {
                     flex: 1;
                     display: flex;
@@ -203,7 +293,11 @@
                     position: relative;
                     height: 100%;
                     &:nth-child(1) {
-                        //background: url(../assets/images/WA-mif-seamless-REVISION.png);
+                        background: url(../assets/images/Sac-rendu-10-07-2020.2-min.jpg);
+                        background-position: center;
+                    }
+                    &:nth-child(2) {
+                        background: url(../assets/images/WA-mif-seamless-REVISION.png);
                     }
                     .content {
                         padding: 20px;
@@ -230,25 +324,88 @@
                         }
 
                     }
-                    .overlay{
+                    .overlay {
                         position: absolute;
-                        background:#1f4046f2;
-                        opacity:0;
+                        background: #1f4046f2;
+                        opacity: 0;
                         top: 0;
                         left: 0;
                         right: 0;
                         bottom: 0;
                         transition: opacity 0.4s ease-in-out;
                     }
-                    &:hover{
-                        cursor: url(../assets/images/cross_100185.svg),auto;
-                        .overlay{
+                    &:hover {
+                        cursor: url(../assets/images/cross_100185.svg), auto;
+                        .overlay {
                             opacity: 1;
                         }
-                        .content{
+                        .content {
                             position: relative;
                             z-index: 1;
-                            article{
+                            article {
+                                opacity: 1;
+                            }
+                        }
+                    }
+                }
+
+            }
+            .horizontal {
+                height: 35%;
+                .inner {
+                    flex: 1;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    position: relative;
+                    height: 100%;
+                    &:nth-child(1) {
+                        background: url(../assets/images/WA-mif-seamless-REVISION.png);
+                    }
+                    .content {
+                        padding: 20px;
+                        text-align: center;
+                        small {
+                            color: #0083A4;
+                            font-family: 'teradeli-light', sans-serif !important;
+                            font-size: 13px;
+                        }
+                        h1 {
+                            color: #fff;
+                        }
+                        p {
+                            font-family: 'teradeli-book', sans-serif !important;
+                            font-size: 20px;
+                            color: rgba(255, 255, 255, .65) !important;
+
+                        }
+                        article {
+                            font-size: 14px;
+                            letter-spacing: 1px;
+                            opacity: 0;
+                            color: #fff;
+                        }
+
+                    }
+                    .overlay {
+                        position: absolute;
+                        background: #1f4046f2;
+                        opacity: 0;
+                        top: 0;
+                        left: 0;
+                        right: 0;
+                        bottom: 0;
+                        transition: opacity 0.4s ease-in-out;
+                    }
+                    &:hover {
+                        cursor: url(../assets/images/cross_100185.svg), auto;
+                        .overlay {
+                            opacity: 1;
+                        }
+                        .content {
+                            position: relative;
+                            z-index: 1;
+                            article {
                                 opacity: 1;
                             }
                         }
