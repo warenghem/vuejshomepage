48d7ef7d6263e717340f3bd009e64ed756badb0d
hasib <hasib.arman.08@gmail.com>
2020-09-26 23:07:29 +0600

    fix modal design

--- front-end/src/components/we-plant-tree-map.vue
+++ front-end/src/components/we-plant-tree-map.vue
@@ -1,6 +1,5 @@
 <template>
     <div>
-        <button class="btn-theme" @click.prevent="openSlide">slide modal</button>
 
         <h2 class="page-title pt-md-5 pt-4">Nous plantons aussi des arbres. Et vous?</h2>
         <h3 class="page-details pb-md-5 py-2 teradeli-light">Pour compenser les émissions de CO2 liées à la création de
@@ -23,27 +22,147 @@
 
         </div>
         <div id="mapSection"></div>
-        <div v-if="slide" class="modal-overlay"></div>
-        <div class="slideArea" v-if="slide">
+        <div class="slideArea">
             <div class="content">
                 <div class="card">
-                    <div class="header">
+                    <div class="card-header d-flex justify-space-between px-3 align-center">
                         <button class="btn-theme">Je plante un arbre gratuitement</button>
                         <v-btn
-                                icon
-                                dark
+                                text
+                                color="black"
                                 @click="closeSlide"
                         >
                             &times;
                         </v-btn>
                     </div>
-                    <div class="body">
-                        <h1>design will be letter more , after your work </h1>
+                    <div class="px-7 py-3 border-top-2 map-modal mgforest">
+                        <v-row>
+                            <v-col lg="6">
+                                <div class="para-title">Fôret</div>
+                                <div class="para-subtitle">Mangrove noire</div>
+                                <div class="para-img">
+                                    <v-img src="../assets/images/specie-mango.jpg"></v-img>
+                                </div>
+                                <v-row>
+                                    <v-col class="text-center ">
+                                        <div class="para-subtitle">20 Kg</div>
+                                        <div class="para-text">Compensation CO2</div>
+                                    </v-col>
+                                    <v-col class="text-center">
+                                        <div class="para-subtitle">25 ans</div>
+                                        <div class="para-text">Durée de vie</div>
+                                    </v-col>
+                                    <v-col class="text-center ">
+                                        <div class="para-subtitle">2 – 10 m</div>
+                                        <div class="para-text">Taille</div>
+                                    </v-col>
+                                </v-row>
+                                <div class="para-subtitle-small">Faune et Flore locale.</div>
+                                <div class="para-text">
+                                    <p>Madagascar dispose de plus de 200 000 espèces de plantes et d’animaux qui
+                                        n’existent nulle part ailleurs dans le monde. 90% des forêts ont été détruites,
+                                        déplaçant des espèces animales entières et les Malgaches.</p>
+
+                                </div>
+                            </v-col>
+                            <v-col lg="6">
+                                <div class="para-title">Projet</div>
+                                <div class="para-subtitle">Projet Paradis, Madagascar</div>
+                                <div class="para-img">
+                                    <v-img src="../assets/images/river.png"></v-img>
+                                </div>
+                                <div class="para-subtitle-small">Incroyable projet. Réussite totale.</div>
+                                <div class="para-text">
+                                    <p>Eden Reforestation Projects a lancé ce programme en 2007 suite à la perte
+                                        critique
+                                        de fôrets.</p>
+
+                                    <p>16 millions de mangroves ont été plantés. Des forêts saines commencent à émerger
+                                        et
+                                        tous les effets négatifs de la déforestation commencent à disparaître.</p>
+                                </div>
+                                <div class="para-subtitle-small">Une communauté locale active. A ne pas oublier.</div>
+                                <div class="para-text">
+                                    <p>Les Vilamatsa avaient hâte de faire partie de cette initiative de reboisement en
+                                        raison du succès d’une plantation à proximité.</p>
+
+                                    <p>Cela leur donne un revenu décent afin qu’ils puissent à nouveau subvenir aux
+                                        besoins
+                                        de leur famille.</p>
+                                </div>
+                            </v-col>
+                            <v-col>
+                                <div class="para-title">Lien Google Map.</div>
+                                <v-img class="py-3" src="../assets/images/map1.png"></v-img>
+                            </v-col>
+                        </v-row>
+
+                    </div>
+                    <div class="px-7 py-3 border-top-2 map-modal indianforest">
+                        <v-row>
+                            <v-col lg="6">
+                                <div class="para-title">Fôret</div>
+                                <div class="para-subtitle">Manguiers</div>
+                                <div class="para-img">
+                                    <v-img src="../assets/images/mango.jpg"></v-img>
+                                </div>
+                                <v-row>
+                                    <v-col class="text-center ">
+                                        <div class="para-subtitle">
+                                            800 Kg
+                                        </div>
+                                        <div class="para-text">Compensation CO2</div>
+                                    </v-col>
+                                    <v-col class="text-center">
+                                        <div class="para-subtitle">100 ans</div>
+                                        <div class="para-text">Durée de vie</div>
+                                    </v-col>
+                                    <v-col class="text-center ">
+                                        <div class="para-subtitle">35 – 45 m</div>
+                                        <div class="para-text">Taille</div>
+                                    </v-col>
+                                </v-row>
+                                <div class="para-subtitle-small">Faune et Flore locale.</div>
+                                <div class="para-text">
+                                    <p>Le parc national de Similipal est le 7e plus grand parc national d’Inde (2 750
+                                        km2), et fait partie du Réseau mondial des réserves de biosphère de l’UNESCO
+                                        depuis 2009. Il abrite le tigre du Bengale, l’éléphant d’Asie, le gaur et le
+                                        chausingha. Ainsi que les plus belles chutes d’eau comme Joranda et Barehipani
+                                        Falls.
+                                    </p>
+
+                                </div>
+                            </v-col>
+                            <v-col lg="6">
+                                <div class="para-title">Projet</div>
+                                <div class="para-subtitle">Des arbres pour les tigres, Inde</div>
+                                <div class="para-img">
+                                    <v-img src="../assets/images/tiger.jpg" alt="tiger"></v-img>
+                                </div>
+                                <div class="para-subtitle-small">Le livre de la jungle 2.</div>
+                                <div class="para-text">
+                                    <p>Le projet vise 50 000 arbres en bordure du parc national de Similipal pour
+                                        enrichir la fôret vierge locale, en particulier pour les tigres du Bengale.</p>
+
+                                </div>
+                                <div class="para-subtitle-small">Et si nous passions les aider ?</div>
+                                <div class="para-text">
+                                    <p>La région du projet est habitée par une variété de communautés tribales (Bhumija,
+                                        Gondas, Kolha… Environ 4 093 jours de travail seront créés pour eux.</p>
+
+                                    <p>L’aéroport le plus proche est celui de Biju Patnaik, situé de 200 km du site. La
+                                        ville la plus proche est Jamshedpur à 105 km.</p>
+                                </div>
+                            </v-col>
+                            <v-col>
+                                <div class="para-title">Lien Google Map.</div>
+                                <v-img class="py-3" src="../assets/images/map2.png"></v-img>
+                            </v-col>
+                        </v-row>
                     </div>
                 </div>
             </div>
         </div>
-
 
     </div>
 </template>
@@ -56,11 +175,6 @@
 
     export default {
         name: "we-plant-tree-map",
-        data() {
-            return {
-                slide: false,
-            }
-        },
         mounted() {
             this.mapCreate();
         },
@@ -200,17 +314,21 @@
                         img.className = "img";
                         conttext.appendChild(img);
 
-                        // // Modal system
-                        // conttext.onclick = function () {
-                        //     $(elt.modal).addClass("is-visible");
-                        //     $("#blackcontent").addClass("blackcontentoverlay");
-                        //     $("body").addClass("blocked");
-                        // };
-                        // dot.onclick = function () {
-                        //     $(elt.modal).addClass("is-visible");
-                        //     $("#blackcontent").addClass("blackcontentoverlay");
-                        //     $("body").addClass("blocked");
-                        // };
+                        // Modal system
+                        conttext.onclick = function () {
+                            document.querySelector('.slideArea').style.visibility = 'visible';
+                            document.querySelector(elt.modal).style.display = 'block';
+                            const el = document.body;
+                            el.classList.add("modal-open");
+                            document.documentElement.style.overflowY='hidden'
+                        };
+                        dot.onclick = function () {
+                            document.querySelector('.slideArea').style.visibility = 'visible';
+                            document.querySelector(elt.modal).style.display = 'block';
+                            const el = document.body;
+                            el.classList.add("modal-open");
+                            document.documentElement.style.overflowY='hidden'
+                        };
                         // append the marker to the map container
                         chart.svgContainer.htmlElement.appendChild(holder);
 
@@ -219,17 +337,14 @@
                 });
             },
 
-            openSlide() {
-                this.slide = true;
-                const el = document.body;
-                el.classList.add("modal-open")
-            },
-
 
             closeSlide() {
-                this.slide = false;
+                document.querySelector('.slideArea').style.visibility = 'hidden';
+                document.querySelector('.indianforest').style.display = 'none';
+                document.querySelector('.mgforest').style.display = 'none';
                 const el = document.body;
                 el.classList.remove('modal-open');
+                document.documentElement.style.overflowY='auto'
             }
         },
 
@@ -248,42 +363,88 @@
     }
 
     .slideArea {
-        height: 100%;
+        position: fixed;
         top: 0;
         bottom: 0;
-        right: 0 !important;
-        position: fixed;
-        z-index: 100000 !important;
-        transition: right 0.4s;
-        background: #fff;
-        margin-top: 0 !important;
-        padding: 0;
-        overflow: auto;
-        @media only screen and (min-width: 1291px) {
-            width: 50vw !important;
-        }
-
-        @media only screen and (max-width: 1290px) {
-            width: 70vw !important;
-        }
-        @media only screen and (max-width: 48em) {
-            width: 100% !important;
-        }
-
-    }
-
-    .modal-overlay {
-        background-color: #000;
-        position: fixed;
         left: 0;
         right: 0;
-        top: 0;
-        bottom: 0;
-        opacity: 0.5;
-        transition: opacity 0.5s, visibility 0s linear 0.5s;
-        -webkit-transition: opacity 0.5s, visibility 0s linear 0.5s;
-        z-index: 100000;
+        height: 100%;
+        z-index: 120000;
+        background: #0000007d;
+        display: flex;
+        flex-direction: row;
+        visibility: hidden;
+        justify-content: flex-end;
+
+        .card-header {
+            position: fixed;
+            z-index: 1;
+            background: white;
+        }
+
+        .content {
+            background: #fff;
+            max-height: 100%;
+            overflow-y: scroll;
+            @media only screen and (min-width: 1291px) {
+                width: 50vw !important;
+                .card-header{
+                     width: 49vw !important;
+                }
+            }
+
+            @media only screen and (max-width: 1290px) {
+                width: 70vw !important;
+                .card-header{
+                     width: 69vw !important;
+                }
+            }
+            @media only screen and (max-width: 48em) {
+                width: 100% !important;
+                .card-header{
+                     width: 99vw !important;
+                }
+            }
+        }
+
+
     }
 
+    .map-modal {
+        display: none;
+
+        .para-title {
+            font-weight: 700;
+            margin-bottom: 5px;
+            color: #888;
+            font-size: 16px;
+        }
+
+        .para-subtitle {
+            font-size: 18px;
+            font-family: 'teradeli-medium', sans-serif;
+            line-height: 1rem;
+            color: #153038;
+            margin: 0;
+        }
+
+        .para-subtitle-small {
+            font-size: 15px;
+            font-family: 'teradeli-medium', sans-serif;
+            line-height: 1rem;
+            color: #153038;
+            margin: 0;
+        }
+
+        .para-img {
+            padding-bottom: 1rem;
+            padding-top: 1rem;
+        }
+
+        .para-text {
+            color: #888;
+            font-size: 14px;
+        }
+    }
 
 </style>
